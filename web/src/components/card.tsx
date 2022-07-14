900b5037c7fbd3dee571ba6237c625efe93b957a
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2022-07-14 15:42:05 +0200

    typing tailwind config

--- web/src/components/card.tsx
+++ web/src/components/card.tsx
@@ -2,27 +2,29 @@ import classnames from "classnames"
 import { ComponentPropsWithRef, ComponentType, createElement, ElementType, FC, ForwardedRef, forwardRef } from "react"
 
 type CardProps<T extends ElementType = ElementType> = ComponentPropsWithRef<T> & {
-  component?: T | ComponentType
-  light?: boolean
+    component?: T | ComponentType
+    light?: boolean
 }
 
+
+
 export const Card: FC<CardProps> = forwardRef(
-  ({ className, children, component = "div", light, ...props }, ref: ForwardedRef<ElementType>) => {
-    return createElement<CardProps>(
-      component,
-      {
-        className: classnames(
-          "bg-white  flex flex-col border rounded-sm m-3 dark:!border-gray-300/20",
-          light && "dark:bg-gray-800",
-          !light && "dark:bg-black",
-          className
-        ),
-        ref,
-        ...props,
-      },
-      children
-    )
-  }
+    ({ className, children, component = "div", light, ...props }, ref: ForwardedRef<ElementType>) => {
+        return createElement<CardProps>(
+            component,
+            {
+                className: classnames(
+                    "bg-white flex flex-col border rounded-sm m-3 dark:!border-gray-300/20 ",
+                    light && "dark:bg-gray-800",
+                    !light && "dark:bg-black",
+                    className
+                ),
+                ref,
+                ...props,
+            },
+            children
+        )
+    }
 )
 
 Card.displayName = "Card"
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,29 +1,30 @@
 const colors = require("tailwindcss/colors")
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin")
 
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
-  content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: colors.fuchsia["600"],
-        "primary-light": colors.fuchsia["400"],
-        "instagradient": "url(#insta-gradient-1)",
-        "linear-instagradient": "linear-gradient(#6559ca, #bc318f 30%, #e33f5f 50%, #f77638 70%, #fec66d 100%);"
-      },
-      maxWidth: {
-        "main-content": "975px",
-      },
-      boxShadow: {
-        "insta": "0 0 5px 1px rgba( .1, .1, .1 ,.0975)",
-        "camera": "0 0px 7px 2px rgb(0 0 0 / 0.25)"
-      }
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: colors.fuchsia["600"],
+                "primary-light": colors.fuchsia["400"],
+                instagradient: "url(#insta-gradient-1)",
+                "linear-instagradient":
+                    "linear-gradient(#6559ca, #bc318f 30%, #e33f5f 50%, #f77638 70%, #fec66d 100%);",
+            },
+            maxWidth: {
+                "main-content": "975px",
+            },
+            boxShadow: {
+                insta: "0 0 5px 1px rgba( .1, .1, .1 ,.0975)",
+                camera: "0 0px 7px 2px rgb(0 0 0 / 0.25)",
+            },
+        },
     },
-  },
-  plugins: [
-    plugin(function ({ addVariant }) {
-      // Add a `third` variant, ie. `third:pb-0`
-      addVariant('child', '& > *')
-    })
-  ]
+    plugins: [
+        plugin(({ addVariant }) => {
+            addVariant("child", "& > *")
+        }),
+    ],
 }
